c85eb0161cda79de409d8167ad18728243e658b4
myy <1937425001@qq.com>
2020-06-23 09:19:57 +0800

    删除了重复文件

--- lwj-flowable-master/flowable/docs/readme.txt
+++
@@ -1,3 +0,0 @@
-1、启动ui
-cnpm install
-npm run start:no-mock
--- lwj-flowable-master/flowable/flowable-manager-ui/src/pages/tasks/services/userService.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request';
-
-export async function getUserList(params: any): Promise<any> {
-  return request(`/server/rest/user/getUserList`, {
-    method: 'GET',
-    params: params,
-  });
-}
